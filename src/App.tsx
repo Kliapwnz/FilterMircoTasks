e7e7341ec255001d60ffa9fe8e9f5b5ca262f709
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-11 14:47:44 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [money, setMoney] = useState([
+        {banknote: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknote: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknote: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknote: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknote: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknote: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknote: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknote: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+    return (
+       <ul>
+           {money.map((objFromMoneyArr, index)=>{
+               return (
+                   <li key={index}>
+                       <span>{objFromMoneyArr.banknote}</span>
+                       <span>{objFromMoneyArr.value}</span>
+                       <span>{objFromMoneyArr.number}</span>
+                   </li>
+               )
+           })}
+       </ul>
+    );
 }
 
 export default App;
